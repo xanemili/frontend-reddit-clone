d817df268888f6136a7f00fe100eb03133d0e68f
Xander <emili.xander@gmail.com>
2020-11-23 00:58:23 -0900

    post retrieval from the database

--- react-app/src/components/subreddit/Subreddit.jsx
+++ react-app/src/components/subreddit/Subreddit.jsx
@@ -1,18 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Post from './Post'
 
 const Subreddit = () => {
 
   const [subreddit, setSubreddit] = useState({})
-  const [postList, setPostList] = useState({})
+  const [posts, setPosts] = useState([])
+  const [errors, setErrors] = useState('')
+  const [postErrors, setPostErrors] = useState('')
   const { subredditName } = useParams();
 
   useEffect(() => {
     (async () => {
       const response = await fetch(`/api/subreddits/r/${subredditName}`)
       const subreddit = await response.json();
-      console.log(subreddit)
-      setSubreddit(subreddit);
+      if(!subreddit.errors) {
+        setSubreddit(subreddit.subreddit);
+      } else {
+        setErrors(subreddit.errors);
+      }
+    })();
+    console.log(errors)
+  }, [subredditName])
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`/api/posts/r/${subredditName}`)
+      const posts = await response.json();
+      if (!posts.errors) {
+        setPosts(posts.posts)
+      } else {
+        setPostErrors(posts.errors);
+      }
     })();
   }, [subredditName])
 
@@ -20,9 +39,22 @@ const Subreddit = () => {
     return null;
   }
 
+  const postComponents = posts.map((post) => {
+    return (
+      <li key={post.id}>
+        <Post title={post.title} type={post.type} content={post.content}/>
+      </li>
+    );
+  })
+
   return (
     <div>
-      {/* <Post /> */}
+      {errors ? <div>{errors}</div> : ''}
+      {console.log(errors)}
+      <h1>{subreddit.name}</h1>
+      <div>About: {subreddit.about}</div>
+      <div>Rules: {subreddit.rules}</div>
+      <ul>{postComponents}</ul>
     </div>
   )
 }
