377d96f2b8c7feccf418dcaca4184905f5ec3fbe
Derek <derekkiminho@gmail.com>
2020-11-25 10:29:40 -0800

    Updated Users model to have the method to_joined_dict and created at for users

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
-from app.models import User
+from app.models import User, Subreddit, Post
 
 user_routes = Blueprint('users', __name__)
 
@@ -15,8 +15,8 @@ def users():
 @user_routes.route('/<int:id>', methods=['GET'])
 @login_required
 def user(id):
-    user = User.query.get(id)
-    return user.to_dict()
+    user = User.query.join(Subreddit).join(Post).filter(User.id == id).one()
+    return user.to_joined_dict()
 
 
 # @user_routes.route('/<int:id>', methods=['DELETE'])
