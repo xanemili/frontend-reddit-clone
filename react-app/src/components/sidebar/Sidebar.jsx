16ddbe227fdd2ab8ddbcaa338f9dbfaae4af2174
Christian Lovern <lovernchristian@gmail.com>
2020-11-25 19:08:38 -0500

    Sidebar top comminuties complete and user info showing in nav

--- react-app/src/components/sidebar/Sidebar.jsx
+++ react-app/src/components/sidebar/Sidebar.jsx
@@ -1,26 +1,48 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import {NavLink} from 'react-router-dom';
+import sideContent from '../../services/sidebar'
 
-const Sidebar= () => {
+const Sidebar= ({ username }) => {
 
-  return(
-    <div>
+  const [followed, setFollowed] = useState({});
+  const [topSubs, setTopSubs] = useState([]);
+
+  const updateValue = (func) => async (e) => {
+    await func(e.target.value)
+  }
+  
+
+  useEffect(() => {
+    (async () => {
+      let subResults = await sideContent(username)
+      console.log(subResults);
+        if(subResults) {
+            setTopSubs(subResults.top_subreddits)
+          }
+        })();
+      }, [])
+      
+      
+    return(
+      <div>
+      {console.log(typeof topSubs)}
       <div className="home-sidebar__container">
-          <div className="home-sidebar__followed">
+          {/* <div className="home-sidebar__followed">
             <h4>Followed Communities</h4>
           </div>
           <ul>
-            <li>/r/one</li>
-            <li>/r/two</li>
-            <li>/r/three</li>
-          </ul>
+            {topSubs.map((sub) => {
+              return <li>/r/{sub.name}</li>
+            })}
+          </ul> */}
           <div className="home-sidebar__top">
             <h4>Top Communities</h4>
           </div>
           <ul>
-            <li>/r/four</li>
-            <li>/r/five</li>
-            <li>/r/six</li>
+          {topSubs.map((sub) => {
+              return <li key = {sub.id}>/r/{sub.name}</li>
+            })}
           </ul>
           <NavLink style={{ width: 'fit-content', textDecoration: 'none' }} to="/subreddits/create" exact={true} activeClassName="active">
           <div className="create__btn">
