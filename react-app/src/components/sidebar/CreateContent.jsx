f404af64d8d6c525c7d0459f3df6b685d64cc372
Christian Lovern <lovernchristian@gmail.com>
2020-11-25 19:50:34 -0500

    sidebar showing members

--- react-app/src/components/sidebar/CreateContent.jsx
+++ react-app/src/components/sidebar/CreateContent.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
-
 const CreateContent = ({ name, about, created, rules, subCount }) => {
 
   let ruleList = rules.split('#')
