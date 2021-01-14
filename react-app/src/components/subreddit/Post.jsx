39c7720f68d6c699fe7ee3ca0b64fb6c36207842
Derek <derekkiminho@gmail.com>
2021-01-14 10:20:44 -0800

    Added updating number of comments for posts

--- react-app/src/components/subreddit/Post.jsx
+++ react-app/src/components/subreddit/Post.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom'
+import {getComments} from '../../services/comments'
 
 
 const Post = ({title, type, content, username, subreddit, created_on, id, userId }) => {
+  const [comments, setComments] = useState(0)
+
+  useEffect(() => {
+    (async () => {
+      const response = await getComments(id)
+      setComments(response.comments.length)
+    })()
+  }, [])
+
 
   if (type === 'image'){
     return(
@@ -11,11 +21,15 @@ const Post = ({title, type, content, username, subreddit, created_on, id, userId
         <div className='post_header'>
           <a href={`/r/${subreddit}`}>{`r/${subreddit}`}</a> <span>Posted by</span> <a href={`/users/${userId}`}>{`${username}`}</a>  created on {created_on}
         </div>
-        <Link to={`/r/${subreddit}/post/${id}`}>
-          <img className="post-img" src={content}/>
-        </Link>
+          <Link to={`/r/${subreddit}/post/${id}`}>
+            <img className="post-img" src={content}/>
+          </Link>
         <div className='meta-area'>
-            Comments
+          <Link to={`/r/${subreddit}/post/${id}`}>
+            <div>
+              {comments} {' '} Comments
+            </div>
+          </Link>
         </div>
       </div>
     )
@@ -32,9 +46,11 @@ const Post = ({title, type, content, username, subreddit, created_on, id, userId
           <a href={`/r/${subreddit}`}>{`r/${subreddit}`}</a> <span>Posted by</span> <a href={`/users/${userId}`}>{`${username}`}</a>  created on {created_on}
         </div>
         <div className='comments'>
-          <div className='meta-area'>
-            Comments
-          </div>
+          <Link to={`/r/${subreddit}/post/${id}`}>
+            <div>
+              {comments} {' '} Comments
+            </div>
+          </Link>
         </div>
     </div>
   )
